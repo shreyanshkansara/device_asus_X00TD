80ef694a1792b0fb58ec6ec86f974ce586d66c6f
shreyanshkansara <skansara789@gmail.com>
2022-04-11 14:57:28 +0000

    X00TD: ProjectAwaken bringup
    Signed-off-by: shreyanshkansara <skansara789@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/awaken_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug
-
+    awaken_X00TD-userdebug \
+    awaken_X00TD-eng
