4b9bce98e87a797601abf19667a83271005ee2b1
shreyanshkansara <skansara789@gmail.com>
2022-08-24 17:26:04 +0000

    X00TD: ConquerOS bringup
    Signed-off-by: shreyanshkansara <skansara789@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/awaken_X00TD.mk
+    $(LOCAL_DIR)/conquer_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    awaken_X00TD-userdebug \
-    awaken_X00TD-eng
+    conquer_X00TD-userdebug \
+    conquer_X00TD-eng
